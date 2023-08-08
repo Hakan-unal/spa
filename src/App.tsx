7acf6e65d741116c536cdeec912f876aef9c6e26
Con Sınov <hakanunal_1996@hotmail.com>
2023-08-08 17:37:13 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,79 @@
-import { Row, Typography, Divider } from 'antd';
-
+import { Row, Typography, Col, Image, Switch } from 'antd';
+import { title1, title2, title3, title4, paragraph1, paragraph2, paragraph3, paragraph4, paragraph5, paragraph6, paragraph7, paragraph8, paragraph9 } from "./language/language"
+import { useState } from 'react';
 
 const { Title, Paragraph, Text, Link } = Typography;
 
 
 const App = () => {
-  const blockContent = "test"
+  const [translate, setTranslate] = useState<boolean>(false)
+  const blockContent = "test ajkshd jkash jkashd jkashdjk ahsdjk ashd kjas "
 
   return (<Row>
-    <Typography>
-      <Title>Introduction</Title>
-      <Paragraph>
-        In the process of internal desktop applications development, many different design specs and
-        implementations would be involved, which might cause designers and developers difficulties and
-        duplication and reduce the efficiency of development.
-      </Paragraph>
-      <Paragraph>
-        After massive project practice and summaries, Ant Design, a design language for background
-        applications, is refined by Ant UED Team, which aims to{' '}
-        <Text strong>
-          uniform the user interface specs for internal background projects, lower the unnecessary
-          cost of design differences and implementation and liberate the resources of design and
-          front-end development
-        </Text>
-        .
-      </Paragraph>
-      <Title level={2}>Guidelines and Resources</Title>
-      <Paragraph>
-        We supply a series of design principles, practical patterns and high quality design resources
-        (<Text code>Sketch</Text> and <Text code>Axure</Text>), to help people create their product
-        prototypes beautifully and efficiently.
+    <Col sm={24}>
+      <Switch unCheckedChildren={<div>TR</div>} checkedChildren={<div>EN</div>} onChange={(checked) => setTranslate(checked)} />
+    </Col>
+
+    <Col sm={24}>
+
+      <Title level={1}>{title1(translate)}</Title>
+
+      <Paragraph >
+        {paragraph1(translate)}
       </Paragraph>
 
+    </Col>
+    <Col sm={8}>
+      <Image width={"80%"} src='https://cdn-blog.scalablepath.com/uploads/2023/01/spa-vs-mpa-architecture-1024x576.png' />
+    </Col>
+    <Col sm={16}>
+      <Title level={2}>{title2(translate)}</Title>
+      <Paragraph>
+        {paragraph2(translate)}
+      </Paragraph>
+    </Col>
+
+    <Col sm={24}>
+      <Title level={3}>{title3(translate)}</Title>
       <Paragraph>
         <ul>
           <li>
-            <Link href="/docs/spec/proximity">Principles</Link>
+            {paragraph3(translate)}
           </li>
           <li>
-            <Link href="/docs/spec/overview">Patterns</Link>
+            {paragraph4(translate)}
           </li>
           <li>
-            <Link href="/docs/resources">Resource Download</Link>
+            {paragraph5(translate)}
+          </li>
+          <li>
+            {paragraph6(translate)}
           </li>
         </ul>
       </Paragraph>
+    </Col>
 
-      <Paragraph>
-        Press <Text keyboard>Esc</Text> to exit...
-      </Paragraph>
+    <Col xs={{ span: 8, offset: 8 }}>
+      <Image width={"100%"} src='https://cdn-blog.scalablepath.com/uploads/2022/12/spa-va-mpa-key-features-1024x800.png' />
+    </Col>
 
-      <Divider />
-
-      <Title>介绍</Title>
-      <Paragraph>
-        蚂蚁的企业级产品是一个庞大且复杂的体系。这类产品不仅量级巨大且功能复杂，而且变动和并发频繁，常常需要设计与开发能够快速的做出响应。同时这类产品中有存在很多类似的页面以及组件，可以通过抽象得到一些稳定且高复用性的内容。
-      </Paragraph>
-      <Paragraph>
-        随着商业化的趋势，越来越多的企业级产品对更好的用户体验有了进一步的要求。带着这样的一个终极目标，我们（蚂蚁集团体验技术部）经过大量的项目实践和总结，逐步打磨出一个服务于企业级产品的设计体系
-        Ant Design。基于<Text mark>『确定』和『自然』</Text>
-        的设计价值观，通过模块化的解决方案，降低冗余的生产成本，让设计者专注于
-        <Text strong>更好的用户体验</Text>。
-      </Paragraph>
-      <Title level={2}>设计资源</Title>
-      <Paragraph>
-        我们提供完善的设计原则、最佳实践和设计资源文件（<Text code>Sketch</Text> 和
-        <Text code>Axure</Text>），来帮助业务快速设计出高质量的产品原型。
-      </Paragraph>
-
+    <Col sm={24}>
+      <Title level={3}>{title4(translate)}</Title>
       <Paragraph>
         <ul>
           <li>
-            <Link href="/docs/spec/proximity-cn">设计原则</Link>
+            {paragraph7(translate)}
           </li>
           <li>
-            <Link href="/docs/spec/overview-cn">设计模式</Link>
+            {paragraph8(translate)}
           </li>
           <li>
-            <Link href="/docs/resources-cn">设计资源</Link>
+            {paragraph9(translate)}
           </li>
         </ul>
       </Paragraph>
+    </Col>
 
-      <Paragraph>
-        <blockquote>{blockContent}</blockquote>
-        <pre>{blockContent}</pre>
-      </Paragraph>
-
-      <Paragraph>
-        按<Text keyboard>Esc</Text>键退出阅读……
-      </Paragraph>
-    </Typography>
   </Row>
   )
 }
---
+++ src/language/language.ts
@@ -0,0 +1,55 @@
+import en from "./en.json"
+import tr from "./tr.json"
+
+export const title1 = (translate: boolean) => {
+    return translate ? en.title1 : tr.title1
+}
+
+export const title2 = (translate: boolean) => {
+    return translate ? en.title2 : tr.title2
+}
+
+export const title3 = (translate: boolean) => {
+    return translate ? en.title3 : tr.title3
+}
+
+export const title4 = (translate: boolean) => {
+    return translate ? en.title4 : tr.title4
+}
+
+
+export const paragraph1 = (translate: boolean) => {
+    return translate ? en.paragraph1 : tr.paragraph1
+}
+
+export const paragraph2 = (translate: boolean) => {
+    return translate ? en.paragraph2 : tr.paragraph2
+}
+
+export const paragraph3 = (translate: boolean) => {
+    return translate ? en.paragraph3 : tr.paragraph3
+}
+
+export const paragraph4 = (translate: boolean) => {
+    return translate ? en.paragraph4 : tr.paragraph4
+}
+
+export const paragraph5 = (translate: boolean) => {
+    return translate ? en.paragraph5 : tr.paragraph5
+}
+
+export const paragraph6 = (translate: boolean) => {
+    return translate ? en.paragraph6 : tr.paragraph6
+}
+
+export const paragraph7 = (translate: boolean) => {
+    return translate ? en.paragraph7 : tr.paragraph7
+}
+
+export const paragraph8 = (translate: boolean) => {
+    return translate ? en.paragraph8 : tr.paragraph8
+}
+
+export const paragraph9 = (translate: boolean) => {
+    return translate ? en.paragraph9 : tr.paragraph9
+}
